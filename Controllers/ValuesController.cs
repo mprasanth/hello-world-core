41642e77a1fe2454046e182fbd414d3225ca22cb
Prasanth Maddali <maddali.prasanth@gmail.com>
2020-12-20 17:39:20 +0800

    Update1

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace hello_world_core.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        public string Index() => $"Hi this is Prasanth";
+    }
+}
